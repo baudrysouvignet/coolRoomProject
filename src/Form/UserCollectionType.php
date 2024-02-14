898d250fbaa9048cb963a4fdf2c746cf45a21013
baudrysouvignet <baudrysouvignet07@gmail.com>
2024-02-14 08:33:13 +0100

    Add collection Entity

---
+++ src/Form/UserCollectionType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\User;
+use App\Entity\UserCollection;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserCollectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('product', EntityType::class, [
+                'class' => Product::class,
+                'choice_label' => 'title',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserCollection::class,
+        ]);
+    }
+}
